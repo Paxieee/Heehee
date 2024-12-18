7c187ab6550bbba90721b3aedee469a1f16f0e28
Paxie P <platebbb@outlook.com>
2024-12-17 23:03:17 -0600

    Header and Footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap'
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import Logo from '../assets/Heehee_logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md " collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Heehee</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={Logo} alt='Heehee logo'></img>
+                    Heehee
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls='cbasic-navbar-nav' />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
