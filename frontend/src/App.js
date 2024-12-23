344b63ff9de6592b43519d437ef75009416e1d16
Paxie P <platebbb@outlook.com>
2024-12-23 00:54:37 -0600

    individual product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreens from './screens/HomeScreens'
-
 
 
 const App = () => {
@@ -12,7 +11,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreens />
+          <Outlet />
         </Container>
       </main>
       <Footer />
