08af167641dfb4639374238b83a7e4e8f94278b6
Paxie P <platebbb@outlook.com>
2024-12-28 22:43:48 -0600

    Mongo set up

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
+// import products from '../products';
 
 
 const HomeScreens = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+
   return (
     <>
         <h1>Latest Products</h1>
