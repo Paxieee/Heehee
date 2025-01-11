13980d09981eb0ec48c07a91c58c95fcda1b3085
Paxie P <platebbb@outlook.com>
2025-01-11 15:14:33 -0600

    things before cartscreen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Badge, Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap';
+import {useSelector} from 'react-redux';
 import Logo from '../assets/Heehee_logo.png';
 
 const Header = () => {
-
+    const { cartItems } = useSelector((state) => state.cart);
+    console.log(cartItems);
 
   return (
     <header>
@@ -25,6 +27,13 @@ const Header = () => {
                         <LinkContainer to='/cart'>
                             <Nav.Link>
                                 <FaShoppingCart /> Cart
+                                {
+                                    cartItems.length > 0 && (
+                                        <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                                            { cartItems.reduce((a, c) => a + c.qty, 0)}
+                                        </Badge>
+                                    )
+                                }
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/login'>
