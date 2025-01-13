525c269368daf602281a4ad839796b39d874cd2b
Paxie P <platebbb@outlook.com>
2025-01-13 17:20:21 -0600

    httptoken set up

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
@@ -7,6 +8,10 @@ const userSchema = new mongoose.Schema({
     isAdmin: {type: Boolean, required: true, default: false},
 },
 {timestamps: true});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const User = mongoose.model('User', userSchema);
 
